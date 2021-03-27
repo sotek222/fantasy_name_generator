8f85568f8a1227691a6b4047708c4cb25cabea2f
Matthew Masiello <sotek222@yahoo.com>
2021-03-27 01:41:40 -0400

    Add initial setup and names

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/main/java/Generator.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class Generator {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+  }
+}
