57f04a8b78489279c16061e1254cb2d0f6e04402
Matthew Masiello <sotek222@yahoo.com>
2021-03-28 19:57:42 -0400

    Add final names

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Fantasy Name Generator: ");
+        System.out.println("Please input a race");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String race = reader.readLine();
+        System.out.println("The race you've chosen is: " + race);
     }
 }
